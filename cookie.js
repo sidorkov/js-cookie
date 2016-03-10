a81cb38c0d3dad46c9fc0f2c83ff0606d828e310
Nikita Sidorkov <sidorkov@me.com>
2016-03-10 09:12:43 +0200

    ini

---
+++ cookie.js
@@ -0,0 +1,40 @@
+var cookie = (function(cookie) {
+	var self = cookie;
+
+	self.get = function(name) {
+		var matches = document.cookie.match(new RegExp(
+		  "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+		));
+		return matches ? decodeURIComponent(matches[1]) : false;
+	};
+
+	// props - объект с свойствами cookie (expires, path, domain, secure)
+	self.set = function(name, value, props) {
+		props = props || {};
+		var exp = props.expires || 30; // by default 30 days
+		if (typeof exp == "number" && exp) {
+			var d = new Date();
+			d.setTime(d.getTime() + exp * 60 *1000); // в минутах
+			exp = props.expires = d;
+		}
+		if(exp && exp.toUTCString) { props.expires = exp.toUTCString(); }
+
+		value = encodeURIComponent(value);
+		var updatedCookie = name + "=" + value;
+		for(var propName in props){
+			updatedCookie += "; " + propName;
+			var propValue = props[propName];
+			if(propValue !== true){ updatedCookie += "=" + propValue; }
+		}
+		document.cookie = updatedCookie;
+		return true;
+	};
+
+	self.delete = function(name) {
+	  self.set(name, "", { expires: -1, path: "/", domain: ".fotovramke.com" });
+	};
+
+
+	return self;
+
+}(cookie || {}));
